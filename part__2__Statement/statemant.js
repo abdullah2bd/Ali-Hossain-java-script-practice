445a2f918339bcce594fd69d9ec26051b5324b10
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-10-12 07:29:31 +0600

    first commit

---
+++ part__2__Statement/statemant.js
@@ -0,0 +1,31 @@
+console.log('Hello');
+document.getElementById('title').innerHTML = 'Hello Word';
+let sum = 50 + 40;
+console.log(sum);
+
+if (sum > 100) {
+  console.log('Expensive');
+} else {
+  console.log('cheep')
+}
+
+
+function sayHello(name) {
+  console.log(`Hello ${name}`);
+}
+
+sayHello('shovo')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
