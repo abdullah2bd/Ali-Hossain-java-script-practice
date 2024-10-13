5d63efed4d4170bf5991ef732103e84610fd530b
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-10-13 08:59:25 +0600

    some practice

--- part__5__Veriable/index.js
+++ part__5__Veriable/index.js
@@ -1,16 +1,28 @@
-console.log('abdullah');
+var a = 10;
+var b = 20;
+var c = a + b;
+
+
+console.log(c + a );
+
+let d = 12;
+d = 22;
+
+
+  console.log(d);
+
+{
+  const h = `i am a student`;
+console.log(h);
+
+}
+console.log(h);
+
+
+{
+  var f = 22;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+}
+console.log(f);
