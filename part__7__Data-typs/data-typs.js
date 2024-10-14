e0ff185628013047a2276683fb2ca8c1ea2af2f9
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-10-14 15:53:37 +0600

    data-type_complete

---
+++ part__7__Data-typs/data-typs.js
@@ -0,0 +1,154 @@
+// there diffrent typs of data typs
+
+// like
+
+//  1. number
+//  2. string
+//  3. bolean
+//  4. araay
+//  5. object
+//  6. undefind
+//  7. null
+//  8. bigInt
+
+
+
+
+// eexample of 1. number
+let a = 10;
+let b = 20;
+// let c = a + b;
+//  let c = a - b;
+// let c = a * b;
+let c = a / b;
+
+
+
+console.log(c);
+
+
+
+
+// eexample of 1. string
+let myName = 'Abdullah';
+let myScndName =  ' Maruf';
+let myFullName = myName + myScndName;
+
+console.log(myName + myScndName);
+console.log(myFullName);
+
+
+
+
+// example of   3. bolean
+let e = 11;
+let f = 12;
+let g = 11;
+
+
+// let h = e != f;
+let h = e !== f;
+// console.log(++e);
+// console.log(--e);
+// console.log(e +=f);
+// console.log(e * e);
+// console.log(g + e);
+
+
+
+
+
+
+
+
+//  example of 4. araay
+let students = ['jahid', 'siam', 'rakib', 'arif', 'joni', 'jamal', 'kamal'];
+
+
+// console.log(students[2]);
+console.log(typeof students);
+console.log( students);
+
+
+
+
+
+
+
+//  example of  5. object
+let person = {
+  fullName:  'Md. Siam Ahmed',
+  Name: 'Siam' ,
+  address: 'Coba-Mosjid',
+  place: 'Companigonj',
+  class: ' Five',
+}
+
+console.log(person);
+
+console.log(person.class);
+
+
+console.log(person.place);
+
+console.log(person.address);
+
+
+
+
+
+
+
+// example of   6. undefind
+
+
+let sum;
+console.log(sum)
+
+
+
+
+
+
+
+
+// example of   6. null
+
+
+
+let we = null;
+
+
+console.log(we);
+
+
+
+
+// example of   6. bigInt
+
+
+
+// let x = 999999999999999n;
+// let y = 100000000000000n;
+
+// let x = BigInt(9999999999999);
+// let y = BigInt(9999999999999);
+
+ 
+let x = BigInt(9999999999999);
+let y = BigInt(9999999999999);
+
+let z = x * y;
+console.log(z);
+
+
+
+
+
+
+
+
+
+
+
+
