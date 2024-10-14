15d34e75e5efcd5905a6679a4407645d89ebfdf8
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-10-14 11:38:53 +0600

    operators

--- part__6__Operator/index.js
+++ part__6__Operator/index.js
@@ -16,53 +16,153 @@
 // Arirhmetic operator
 
 
-let a = 10;
-let b = 20;
-let c = a + b;
-let d = a - b;
-let e = a / b;
-let f = a % b;
-let g = a + b;
+// let a = 10;
+// let b = 20;
+// let c = a + b;
+// let d = a - b;
+// let e = a / b;
+// let f = a % b;
+// let g = a + b;
 
- let h = a + b + c + d + e + f;
-console.log(a);
+//  let h = a + b + c + d + e + f;
+// console.log(a);
 
 
-console.log(b);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+
+// console.log(h);
+
+
+// let userInput = prompt('Enter a number');
+// let number = parseInt(userInput);
+
+
+// if (number % 2 === 0){
+//   console.log(`${number} is even number`);
+// } else {
+//   console.log(`${number} is odd number`);
+// }
+
+//  arithmatic operator
+// let m = 10;
+// let n = 10;
+//let l = m /= n;
+//let l = m += n;
+// let l = m -= n;
+//let l = m *= n;
+// l0et l = m %= n;
+// console.log(l);
+
+// let userInput = prompt('Enter your name');
+// let number = parseInt(userInput);
+
+
+
+
+
+
+
+// let ab = 3;
+// let bc = 4;
+// let ca = ab !== bc;
+
+// console.log(ca);
+
+
+
+
+
+
+
+
+
+
+
+// let a = 3;
+// let b = 4;
+
+
+// let c = ++a;
+// let d= --a;
+// let e= a++;
+// let f= a--;
+
+
+
+// console.log(c);
+
+// console.log(d);
+
+// console.log(e);
+// console.log(f);
+
+
+
+
+// let c = a + b;
+// let d= a < b;
+// let e= a > b;
+// let f= a != b;
+
+
+
+// let c = a += b;
+// let d= a >= b;
+// let e= a <= b;
+// let f= a !== b;
+
+
+// console.log(c);
+
+// console.log(d);
+
+// console.log(e);
+// console.log(f);
+
+
+
+
+let a = 3;
+let b = 4;
+
+
+let c = a || b;
+let d= a && b;
+let e= !( a < b);
+let f= !( a > b);
+
+
 console.log(c);
+
 console.log(d);
+
 console.log(e);
 console.log(f);
-console.log(g);
-
-console.log(h);
 
 
 
 
+let string = "rgre rtht  y54 hbt hrwtbbt 5yh t h th";
+console.log( typeof string)
 
 
 
 
-
-
-
+let strin = 5474527457245;
+console.log( typeof strin)
 
 
 
 
 
 
-
-let userInput = prompt('Enter a number');
-let number = parseInt(userInput);
-
-
-if (number % 2 === 0){
-  console.log(`${number} is even number`);
-} else {
-  console.log(`${number} is odd number`);
-}
+let stri = "rgre rtht  y54 hbt hrwtbbt 5yh t h th";
+console.log( typeof stri)
 
 
 
