8c10c6d473c093044b1ec57a88d4846f99de01a2
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-10-17 20:56:39 +0600

    js

--- part__11__Arrays/array.js
+++ part__11__Arrays/array.js
@@ -14,7 +14,7 @@ language[3] = 'TAilwind css';
 language[5] = 'React js';
 language[6] = 'Vue,js';
 language[7] = 'laravel';
-language[10] = 'laravel';
+language[10] = 'Bootstarp';
 
 // Frist Item
 console.log(language[0]);
@@ -25,14 +25,73 @@ console.log(language[5]);
 console.log(language[language.length - 1]);
 
 
-// console.log(language);
+console.log(language);
 // console.log(language[1]);
 
-console.log(language);
-console.log(language[3]);
-console.log(language.toString());
+// console.log(language);
+// console.log(language[3]);
+// console.log(language.toString());
 
-console.log(language.length);
+// console.log(language.length);
+
+
+
+
+
+const person = [];
+person['fiestName'] = 'Ali';
+person['lasstName'] = 'Hossain';
+person[2] = 35;
+
+console.log(person.length);
+
+console.log(person);
+
+console.log(person[0]);
+
+console.log(person[1]);
+
+
+console.log(person[2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
