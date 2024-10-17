727602410c65badc7fe6966f00251f54701d4eab
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-10-17 20:39:24 +0600

    array practice

---
+++ part__11__Arrays/array.js
@@ -0,0 +1,46 @@
+
+/*
+const lang1 = 'HTML';
+const lang2 = 'CSS';
+const lang3 = 'JAVA SCRIPT';
+const lang4 = 'PHP';
+*/
+// const language2 = new Array('HTML', 'CSS', 'Java script', 'PHP', true, 50 );
+
+const language = ['HTML', 'CSS', 'Java script', 'PHP', true, 50 ];
+
+
+language[3] = 'TAilwind css';
+language[5] = 'React js';
+language[6] = 'Vue,js';
+language[7] = 'laravel';
+language[10] = 'laravel';
+
+// Frist Item
+console.log(language[0]);
+
+console.log(language[5]);
+
+// LAst item
+console.log(language[language.length - 1]);
+
+
+// console.log(language);
+// console.log(language[1]);
+
+console.log(language);
+console.log(language[3]);
+console.log(language.toString());
+
+console.log(language.length);
+
+
+
+
+
+
+
+
+
+
+
