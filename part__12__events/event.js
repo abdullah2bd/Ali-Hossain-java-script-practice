ebadc4a59103056115a1ab1e216b5f6fa4bb2f46
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-10-19 15:13:24 +0600

    js

--- part__12__events/event.js
+++ part__12__events/event.js
@@ -1,6 +1,19 @@
 // events
-function subscribe() {
-  document.getElementById('text').innerHTML = 'Subscribed';
-  document.getElementById('text').innerHTML = 'Subscribed';
-  document.getElementById('text').innerHTML = 'Subscribed';
-}  
+// function subscribe() {
+//   document.getElementById('text').innerHTML = 'Subscribed';
+//   document.getElementById('text').innerHTML = 'Subscribed';
+//   document.getElementById('text').innerHTML = 'Subscribed';
+// }  
+
+
+
+
+function subscribe(){
+    document.getElementById('btn').innerHTML = 'Subscribed';
+
+}
+
+
+
+
+
